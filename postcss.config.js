fcfb1eb5bf69726d92fba72322e28ab8a28fc48b
int0p <closer.fz@gmail.com>
2023-12-24 16:28:35 +0900

    formatting

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+export default {
+	plugins: [
+		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+		tailwindcss(),
+		//But others, like autoprefixer, need to run after,
+		autoprefixer
+	]
+};
